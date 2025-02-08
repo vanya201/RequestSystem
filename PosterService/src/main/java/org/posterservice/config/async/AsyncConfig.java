3524fd8fdb5ebcb0678ac31d714d0bb9c9d2d82e
vanya201 <lenapetrova1905@gmail.com>
2025-02-08 04:31:25 +0200

    added second service

---
+++ PosterService/src/main/java/org/posterservice/config/async/AsyncConfig.java
@@ -0,0 +1,25 @@
+package org.posterservice.config.async;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean
+    @Primary
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("GlobalAsync-");
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ PosterService/src/main/java/org/posterservice/services/UserSearchService.java
@@ -0,0 +1,32 @@
+package org.posterservice.services;
+
+
+import lombok.RequiredArgsConstructor;
+import org.common.models.User;
+import org.posterservice.repositorys.UserSearchRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserSearchService {
+    private final UserSearchRepository userSearchRepository;
+
+    public User searchUserByName(String username) {
+        return userSearchRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User Not Found"));
+    }
+
+    public List<User> searchFriendsByUser(User user, Pageable page) {
+        return userSearchRepository.findByFriendsContains(user, page).getContent();
+    }
+
+    public List<User> searchUsersByName(String username, Pageable page) {
+        Page<User> users = userSearchRepository.findByUsernameLike(username, page);
+        return users.isEmpty() ?
+                userSearchRepository.findSimilarUsernames(username, page).getContent()
+                : users.getContent();
+    }
+}
